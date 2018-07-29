02ca7f6ba538a7b10a1d2c7b38c978ee4880b36d
sjyttkl <695492835@qq.com>
2018-07-29 11:30:35 +0800

    支持向量2

--- 支持向量机/svmm.py
+++ 支持向量机/svmm.py
@@ -137,14 +137,77 @@ class optStruct:
         self.m = np.shape(dataMatIn)[0]
         self.alphas = np.mat(np.zeros((self.m,1)))
         self.b = 0
-        self.eCahce = np.mat(np.zeros((self.m,2)))
+        self.eCache = np.mat(np.zeros((self.m,2))) #误差缓存,第一列是是否有效的标记位，第二位是实际的值
 
-    def calcEk(oS,k):
-        fXk = float(np.multiply(oS.alphas,oS.labelMat).T *(oS.X*oS.X[k,:].T))+oS.b
-        Ek = fXk - float(oS.labelMat[k])
-        return Ek
-    def selectJ(i,oS,Ei):
-        pass
+    #计算误差
+def calcEk(oS,k):
+    fXk= float(np.multiply(oS.alphas,oS.labelMat).T*(oS.X*oS.X[k,:])) +oS.b
+    Ek = fXk-float(oS.labelMat[k])
+    return Ek
+
+def selectJ(i,oS,Ei):   #内循环中的启发式方法
+    maxK=-1;maxDeltaE = 0;Ej = 0;
+    oS.eCache[i] = [1,Ei]
+    validEcacheList = np.nonzero(oS.eCache[:,0].A)[0]
+    if(len(validEcacheList))>1:
+        for k in validEcacheList:
+            if k==i :continue
+            Ek = calcEk(oS,k)
+            deltaE = np.abs(Ei-Ek)
+            if (deltaE >maxDeltaE):
+                maxK = k
+                maxDeltaE = deltaE
+                Ej = Ek
+        return maxK,Ej
+    else:
+        j = selectJrand(i,oS.m)
+        Ej= oS.calcEk(oS,j)
+    return j,Ej
+def updateEk(oS,k):
+    Ek = calcEk(oS,k)
+    oS.eCache[k] = [1,Ek]
+
+def innerL(i,oS):
+    Ei = calcEk(oS,i)
+    if((oS.labelMat[i]*Ei <-oS.tol) and (oS.alphas[i]<oS.c) or ((oS.labelMat[i]*Ei>oS.tol) and (oS.alphas[i]>0))):
+        j,Ej = selectJ(i,oS,Ei)
+        alphaIold = oS.alphas[i].copy()
+        alphaJold =oS.alphas[j].copy()
+        if(oS.labelMat[i]!=oS.labelMat[j]):
+            L= np.max(0,oS.alphas[j] - oS.alphas[i])
+            H = np.min(oS.C,oS.C+oS.alphas[j]-oS.alphas[i])
+        else:
+            L=np.max(0,oS.alphas[j]+oS.alphas[i]-oS.C)
+            H = np.min(oS.C,oS.alphas[j]+oS.alphas[i])
+        if L==H :print("L == H") ;return 0
+        eta = 2.0*oS.X[j,:]*oS.X[i,:].T -oS.X[i,:]*oS.X[i,:].T-oS.X[j,:]*oS.X[j,:].T
+        if eta>=0:print ("eta>0");return 0;
+        oS.alphas[j] -= oS.labelMat[j]*(Ei-Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j],H,L)
+        updateEk(oS,j) #更新误差缓存
+        if(np.abs(oS.alphas[j]-alphaJold) < 0.00001):
+            print("j no moving enough "); return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold-oS.alphas[j])
+        updateEk(oS,i) #更新误差缓存
+
+        b1 = oS.b -Ei-oS.labelMat[i]*(oS.alphas[i] - alphaIold) *oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
+
+        b2 = oS.b -Ei-oS.labelMat[i]*(oS.alphas[i] - alphaIold) *oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
+
+        if(0<oS.alphas[i]) and (oS.C>oS.alphas[i]):
+            oS.b =b1
+        elif (0<oS.alphas[j]) and (oS.C>oS.alphas[j]):
+            oS.b = b2
+        else:
+            oS.b = (b1+b2)/2.0
+        return 1
+    else:
+        return 0
+
+
+
+
+
 
 if __name__=="__main__":
     dataArr,labelArr = loadDataSet('testSet.txt')
