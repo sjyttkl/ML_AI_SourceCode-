d7bbddf053368cca9e43a0378e87c611203ea667
songdongdong2 <songdongdong@jd.com>
2019-01-08 20:52:06 +0800

    归并排序
    Signed-off-by: songdongdong2 <songdongdong@jd.com>

--- 基础算法/merge_sort.py
+++ 基础算法/merge_sort.py
@@ -30,21 +30,23 @@ def merge_sort(arr):
     return merge(left, right, arr.copy())
 
 
-def merge(left, right, merged):
-    left_cursor, right_cursor = 0, 0
-    while left_cursor < len(left) and right_cursor < len(right):
-        # Sort each one and place into the result
-        if left[left_cursor] <= right[right_cursor]:
-            merged[left_cursor + right_cursor] = left[left_cursor]
-            left_cursor += 1
-        else:
-            merged[left_cursor + right_cursor] = right[right_cursor]
-            right_cursor += 1
+def merge(left,right,merged):
+	left_cursor , right_cursor = 0,0
+	while left_cursor < len(left) and right_cursor <len(right):
+		if left[left_cursor] < right[right_cursor] :
+			merged[left_cursor + right_cursor] = left[left_cursor]
+			left_cursor += 1
+		else:
+			merged[left_cursor + right_cursor] = right[right_cursor]
+			right_cursor +=1
 
-    for left_cursor in range(left_cursor, len(left)):
-        merged[left_cursor + right_cursor] = left[left_cursor]
+	for left_cursor in range(left_cursor ,len(left)):
+		merged[left_cursor + right_cursor] = left[left_cursor]
 
-    for right_cursor in range(right_cursor, len(right)):
-        merged[left_cursor + right_cursor] = right[right_cursor]
+	for right_cursor in range(right_cursor,len(right)):
+		merged[left_cursor + right_cursor] = right[right_cursor]
 
-    return merge
+	return merged
+
+a = [1,2,1,4,4,2,0,3,2,8,2,4,5]
+print(merge_sort(a))
